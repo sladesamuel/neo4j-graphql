b150579f4549425da2c4ed66768ad8e157e4bfcf
Samuel Slade <samuel.pjm.slade@gmail.com>
2021-12-17 15:52:02 +0000

    Added neo4j setup

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const { gql, ApolloServer } = require("apollo-server");
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+const neo4j = require("neo4j-driver");
+
+require("dotenv").config();
+
+const typeDefs = gql`
+  type Movie {
+    title: String!
+    released: Int
+    tagline: String
+    actors: [Person] @relationship(type: "ACTED_IN", direction: IN)
+  }
+
+  type Person {
+    name: String!
+    movies: [Movie] @relationship(type: "ACTED_IN", direction: OUT)
+  }
+`;
+
+const {
+  DB_URI: dbUri,
+  DB_USERNAME: username,
+  DB_PASSWORD: password,
+} = process.env;
+
+const driver = neo4j.driver(dbUri, neo4j.auth.basic(username, password));
+
+const { schema } = new Neo4jGraphQL({
+  typeDefs,
+  driver,
+});
+
+const server = new ApolloServer({
+  schema,
+});
+
+server
+  .listen()
+  .then(({ url }) => console.log(`GraphQL server ready on ${url}`));
