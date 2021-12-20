942e111058bccbf3f622872f50ece51a26f1557d
Samuel Slade <samuel.pjm.slade@gmail.com>
2021-12-20 15:48:23 +0000

    Setup default router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,3 +1,15 @@
 import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import Movies from "./pages/Movies"
+import Navigation from "./pages/Navigation"
 
-export default () => <div>Hello, World!</div>
+export default () => (
+  <Router>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Navigation />} />
+        <Route path="/movies" element={<Movies />} />
+      </Route>
+    </Routes>
+  </Router>
+)
---
+++ webapp/src/pages/Movies.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+  <>
+    <h1>Movies</h1>
+
+    <div>TODO: Data</div>
+  </>
+)
